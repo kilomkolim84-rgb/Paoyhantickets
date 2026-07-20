da567778fe8b7db6ad3d29e55fd402933120bc01
kilomkolim84-rgb <kilomkolim84@gmail.com>
2026-07-19 20:23:51 -0500

    Update MainActivity.kt

--- app/src/main/java/com/kilomkolim84rgb/paoyangtickets/MainActivity.kt
+++ app/src/main/java/com/kilomkolim84rgb/paoyangtickets/MainActivity.kt
@@ -31,6 +31,7 @@ import com.google.zxing.qrcode.QRCodeWriter
 import kotlinx.coroutines.delay
 import kotlin.random.Random
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
